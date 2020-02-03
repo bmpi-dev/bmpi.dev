8b449dacdf4d50125925b8f186e9bd4209a95db3
madawei2699 <madawei2699@gmail.com>
2020-02-03 22:39:11 +0800

    fix new post check error on netlify

--- _tools/newpostcheck.js
+++ _tools/newpostcheck.js
@@ -5,23 +5,60 @@ const fetch = require("node-fetch");
 
 (async () => {
 
+    const baseUrl = 'https://www.bmpi.dev';
+    
     // fetch the live data source
-    const response = await fetch("https://www.bmpi.dev/feed/index.json");
+    const response = await fetch(baseUrl + "/feed/index.json");
     if (response.status !== 200) {
         throw new Error("Invalid response status: " + response.status);
     }
 
     const currentFeedPosts = await response.json();
-    console.log("Current feed post is ", currentFeedPosts.latest-post);
+    console.log("Current feed post is ", currentFeedPosts['latest-post']);
 
     // read the locally built version of the data source
     const onlineFeedPosts = JSON.parse(fs.readFileSync("./public/feed/index.json", { encoding: "utf8" }));
-    console.log("Online feed post is ", onlineFeedPosts.latest-post);
+    console.log("Online feed post is ", onlineFeedPosts['latest-post']);
 
     // compare the two
-    if (currentFirstPost.url !== newFirstPost.url) {
+    if (currentFeedPosts['latest-post']['url'] !== onlineFeedPosts['latest-post']['url']) {
         console.log("New post detected, start web push notify!");
         // start web push notify
+        var request = require('request');
+
+        var headers = {
+            'webpushrKey': process.env.webpushrAuthToken,
+            'webpushrAuthToken': process.env.webpushrKey,
+            'Content-Type': 'application/json'
+        };
+
+        var postData = {
+            'title': onlineFeedPosts['latest-post']['title'],
+            'message': onlineFeedPosts['latest-post']['description'],
+            'target_url': baseUrl + onlineFeedPosts['latest-post']['url']
+        }
+
+        var dataString = JSON.stringify(postData);
+
+        console.log(dataString);
+
+        var options = {
+            url: 'https://app.webpushr.com/api/v1/notification/send/all',
+            method: 'POST',
+            headers: headers,
+            body: dataString
+        };
+
+        function callback(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body);
+            } else {
+                console.log("Push new post success")
+            }
+        }
+
+        request(options, callback);
+
     } else {
         console.log("No New post detected!");
     }
