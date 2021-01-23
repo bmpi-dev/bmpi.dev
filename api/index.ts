b2bff56d4df0d942258397bcf010baf2909f1afa
madawei2699 <madawei2699@gmail.com>
2021-01-23 14:05:43 +0800

    update newsletter subscribe style

--- api/index.ts
+++ api/index.ts
@@ -3,28 +3,30 @@ import * as AWS from "aws-sdk";
 import * as awsx from "@pulumi/awsx";
 
 const hits = new aws.dynamodb.Table("bmpi-dev", {
-    attributes: [{ name: "Site", type: "S" }],
-    hashKey: "Site",
+    attributes: [{ name: "BMPI", type: "S" }],
+    hashKey: "BMPI",
     billingMode: "PAY_PER_REQUEST",
 });
 
 const site = new awsx.apigateway.API("api-bmpi-dev-views", {
     routes: [{
-        path: "/",
+        path: "/{route+}",
         method: "GET",
-        eventHandler: async () => {
+        eventHandler: async (event) => {
+            const route = event.pathParameters!["route"];
+            console.log(`Getting count for '${route}'`);
             const dc = new AWS.DynamoDB.DocumentClient();
             const result = await dc.update({
                 TableName: hits.name.get(),
-                Key: { "Site": "ACMECorp" },
+                Key: { id: route },
                 UpdateExpression: "SET Hits = if_not_exists(Hits, :zero) + :incr",
                 ExpressionAttributeValues: { ":zero": 0, ":incr": 1 },
                 ReturnValues:"UPDATED_NEW",
             }).promise();
             return {
                 statusCode: 200,
-                headers: { "Content-Type": "text/html" },
-                body: `<h1>Welcome to ACMECorp!</h1><p>${result.Attributes!.Hits} hits.</p>`,
+                headers: { "Content-Type": "application/json; charset=utf-8" },
+                body: `{"hits":${result.Attributes!.Hits}}`,
             };
         },
     }],
