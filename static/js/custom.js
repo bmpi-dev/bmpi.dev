a35604ca328aacb659e858de308b2ca51e90609e
madawei2699 <madawei2699@gmail.com>
2022-08-25 23:21:54 +0800

    fix bookmark error on mobile && enable it on PC

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,3 +1,10 @@
+// common functions
+window.mobileCheck = function() {
+    let check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+};
+
 // load sw.js
 if("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
@@ -138,7 +145,7 @@ function addQRPart(html) {
 async function html2Img(html) {
     let div = document.createElement('div');
     div.id = 'capture';
-    div.setAttribute('style', 'padding: 30px 20px;background: #000;color: #f7f4cb;font-family: "LXGW WenKai";');
+    div.setAttribute('style', 'padding: 30px 20px;background: #000;color: #f7f4cb;font-family: "LXGW WenKai";max-width: 768px;margin: auto;');
     div.innerHTML = html;
     addDatePart(div);
     let footer = document.createElement('div');
@@ -146,20 +153,29 @@ async function html2Img(html) {
     addAuthorPart(footer);
     addQRPart(footer);
     div.appendChild(footer);
+    let bodyBackup = document.body;
+    if (window.mobileCheck()) {
+        document.body = document.createElement("body");
+    }
     document.body.appendChild(div);
     let canvas = await html2canvas(div, {allowTaint: true, useCORS: true});
+    if (window.mobileCheck()) {
+        document.body = bodyBackup;
+    } else {
+        document.body.removeChild(div);
+    }
     let dataURL = canvas.toDataURL("image/png");
-    const blob = dataURItoBlob(dataURL);
-    const data = {
-        files: [
-            new File([blob], document.querySelector('.title').textContent + '.png', {
-            type: blob.type,
-            }),
-        ],
-        // title: document.title,
-        // text: text,
-    };
     if (navigator.canShare && navigator.canShare(data)) {
+        const blob = dataURItoBlob(dataURL);
+        const data = {
+            files: [
+                new File([blob], document.querySelector('.title').textContent + '.png', {
+                type: blob.type,
+                }),
+            ],
+            // title: document.title,
+            // text: text,
+        };
         try {
             await navigator.share(data);
             ga('gtag_UA_154678195_1.send', {
@@ -183,10 +199,17 @@ async function html2Img(html) {
             umami.trackEvent('bookmark_fail', 'bookmark');
         }
     } else {
-        launch_toast("浏览器不支持此功能");
-        console.log(`Your system doesn't support sharing files.`);
+        let modal = document.getElementById("the-modal");
+        var modalImg = document.getElementById("the-modal-img01");
+        modal.style.display = "block";
+        modalImg.src = dataURL;
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("the-modal-close")[0];
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() { 
+            modal.style.display = "none";
+        }
     }
-    document.body.removeChild(div);
 }
 
 async function oncontroldown(event) {
@@ -216,16 +239,14 @@ function webShare() {
 
     elements.forEach(i => {
         i.onpointerup = ()=>{
-            if (navigator.canShare) {
-                let selection = document.getSelection(), text = selection.toString();
-                if (text !== "") {
-                    let rect = selection.getRangeAt(0).getBoundingClientRect();
-                    let articleY =  document.body.getBoundingClientRect().top;
-                    control.style.top = `calc(${rect.bottom}px - ${articleY}px + 20px)`;
-                    control.style.left = `calc(${rect.left}px + calc(${rect.width}px / 2) - 30px)`;
-                    control['html']= getSelectionHtml(); 
-                    document.body.appendChild(control);
-                }
+            let selection = document.getSelection(), text = selection.toString();
+            if (text !== "") {
+                let rect = selection.getRangeAt(0).getBoundingClientRect();
+                let articleY =  document.body.getBoundingClientRect().top;
+                control.style.top = `calc(${rect.bottom}px - ${articleY}px + 20px)`;
+                control.style.left = `calc(${rect.left}px + calc(${rect.width}px / 2) - 30px)`;
+                control['html']= getSelectionHtml(); 
+                document.body.appendChild(control);
             }
         }
     });
@@ -251,12 +272,6 @@ if( document.readyState !== 'loading' ) {
 }
 
 // remove dashboard iframe on mobile
-
-// window.mobileCheck = function() {
-//     let check = false;
-//     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-//     return check;
-// };
 
 // var dashboard = document.getElementById("dashboard_iframe");
 
